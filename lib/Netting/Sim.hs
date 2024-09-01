cb6747dc00d3ab2a953c9ee98f02ad1d3181ebe4
aleksander junge <ajunge123@gmail.com>
2024-09-01 15:44:53 +0200

    refactor single txn arb

--- lib/Netting/Sim.hs
+++ lib/Netting/Sim.hs
@@ -17,13 +17,8 @@ genInRange (from, to) = getStdRandom (randomR (from, to))
 genSingleArbitrageTxn :: String -> State -> Transaction
 genSingleArbitrageTxn name s@(amms, users) =
   let input_amounts = map singleAmmArbitrage amms in
-    let output_amounts = zipWith payout amms input_amounts in 
-      let transactions = zipWith (\x y -> Transaction name x y) input_amounts output_amounts in 
-        let gains = map (flip gain s) transactions in 
-          transactions !! fromMaybe 0 (elemIndex (maximum gains) gains)
-  where 
-    payout (AMM (t0, r0) (t1, r1)) (t2, v0) -- payout that preserves CFMM properties
-      | t0 == t2  = (t1, (r1 * v0) / (r0 + v0)) 
-      | otherwise = (t0, (r0 * v0) / (r1 + v0))
+    let transactions = map (\(x, y) -> Transaction name x y) input_amounts in 
+      let gains = map (flip gain s) transactions in 
+        transactions !! fromMaybe 0 (elemIndex (maximum gains) gains)
 
 -- TODO: Add more functions to generate transactions
--- lib/Netting/Worth.hs
+++ lib/Netting/Worth.hs
@@ -38,15 +38,19 @@ gain txn@(Transaction name _ _) s@(_, users) =
                         Just $ worth' - worth
 
 -- considers the arbitrage game on a single CFMM AMM (https://lmcs.episciences.org/10504/pdf, lemma 6.4)
--- returns the 'from swap amount' that brings AMM back into equilibrium with oracle rate
-singleAmmArbitrage :: AMM -> TokenAmt
-singleAmmArbitrage (AMM (t0, r0) (t1, r1)) =
+-- returns the 'input swap amount' that brings AMM back into equilibrium with oracle rate
+singleAmmArbitrage :: AMM -> (TokenAmt, TokenAmt)
+singleAmmArbitrage amm@(AMM (t0, r0) (t1, r1)) =
     if r1 / r0 > oracleXRate t0 t1 then 
         -- t1 is cheap compared to oracle rate, thus swap(t0, t1) gives pos gain
         let v0 = sqrt (oracleXRate t1 USDC / oracleXRate t0 USDC * r0 * r1) - r0 in 
-            (t0, v0)
+            ((t0, v0), payout amm (t0, v0))
     else 
         -- t0 is cheap compared to oracle rate, thus swap(t1, t0) gives pos gain
         let v1 = sqrt (oracleXRate t0 USDC / oracleXRate t1 USDC * r1 * r0) - r1 in 
-            (t1, v1)
+            ((t1, v1), payout amm (t1, v1))
+    where 
+        payout (AMM (t0, r0) (t1, r1)) (t2, v0) -- payout that preserves CFMM properties
+            | t0 == t2  = (t1, (r1 * v0) / (r0 + v0)) 
+            | otherwise = (t0, (r0 * v0) / (r1 + v0))
             
